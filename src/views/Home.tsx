9dfcab80b590dbdb669a3d95e35d765e40e76df5
william-cesar <william.cesar.mg@hotmail.com>
2024-04-28 00:32:43 -0300

    feat: add step card to home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,11 @@
 import Introduction from '@/components/_custom/Introduction';
+import StepCard from '@/components/_custom/StepCard';
 
 const Home = () => {
   return (
     <div className='flex flex-col gap-8'>
       <Introduction />
+      <StepCard />
     </div>
   );
 };
