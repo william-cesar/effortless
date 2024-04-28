03cbb970c9c9a33a4ec7e837087c6aa9fd27c41c
william-cesar <william.cesar.mg@hotmail.com>
2024-04-28 00:30:54 -0300

    feat: create action bar for card

---
+++ src/components/_custom/StepCard/ActionBar.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { CardFooter } from '@/components/ui/card';
+
+type ActionBarProps = {
+  hasNext: boolean;
+  hasPrevious: boolean;
+  onEvent: (event: string) => void;
+};
+
+const ActionBar = (props: ActionBarProps) => {
+  return (
+    <CardFooter className='flex gap-4 sm:justify-end max-sm:flex-col'>
+      {props.hasPrevious ? (
+        <Button
+          className='max-sm:w-full'
+          variant='outline'
+          onClick={() => props.onEvent('prev')}
+        >
+          Previous
+        </Button>
+      ) : null}
+      {props.hasNext ? (
+        <Button className='max-sm:w-full' onClick={() => props.onEvent('next')}>
+          Next
+        </Button>
+      ) : null}
+    </CardFooter>
+  );
+};
+
+export default ActionBar;
