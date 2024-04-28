aa262a7e961c588684766f0019ecdf78a5799749
william-cesar <william.cesar.mg@hotmail.com>
2024-04-27 21:12:30 -0300

    feat: create home view

---
+++ src/views/Home.tsx
@@ -0,0 +1,110 @@
+const Home = () => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sapiente eligendi dolorem fuga natus minus exercitationem commodi maxime
+        enim, adipisci, inventore reiciendis facere ab et libero quia
+        perspiciatis, recusandae reprehenderit.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
