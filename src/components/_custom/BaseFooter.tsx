2f955da31dc94a31a355058507bd4ad16e8c8e98
william-cesar <william.cesar.mg@hotmail.com>
2024-04-27 21:09:12 -0300

    feat: create base footer layout

---
+++ src/components/_custom/BaseFooter.tsx
@@ -0,0 +1,9 @@
+const BaseFooter = () => {
+  return (
+    <footer className='py-4 px-8 bg-secondary h-20 fixed bottom-0 w-full flex justify-end items-center text-sm text-muted-foreground'>
+      Effortless - version 0.0.1
+    </footer>
+  );
+};
+
+export default BaseFooter;
