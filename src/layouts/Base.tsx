7a3474f238fb9b8f1c0a155a69022c53f37fd5be
william-cesar <william.cesar.mg@hotmail.com>
2024-04-27 21:10:01 -0300

    feat: create base layout

---
+++ src/layouts/Base.tsx
@@ -0,0 +1,17 @@
+import BaseFooter from '@/components/_custom/BaseFooter';
+import BaseHeader from '@/components/_custom/BaseHeader';
+import Home from '@/views/Home';
+
+const BaseLayout = () => {
+  return (
+    <div className='flex flex-col'>
+      <BaseHeader />
+      <main className='px-8 py-10 my-20 min-h-area max-w-screen-lg overflow-y-auto mx-auto'>
+        <Home />
+      </main>
+      <BaseFooter />
+    </div>
+  );
+};
+
+export default BaseLayout;
