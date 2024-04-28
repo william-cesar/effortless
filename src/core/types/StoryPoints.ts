702d0d49e0547afeeefd2948db31187b748b2c8c
william-cesar <william.cesar.mg@hotmail.com>
2024-04-28 19:11:38 -0300

    feat: add story points types

---
+++ src/core/types/StoryPoints.ts
@@ -0,0 +1,31 @@
+export type FactorsValues = {
+  complexity: number;
+  uncertainty: number;
+  effort: number;
+};
+
+export type Message = {
+  title: string;
+  description: string;
+  type: 'success' | 'warning' | 'error';
+};
+
+export type StoryPointsMessages = {
+  mediumComplexity: Message;
+  highComplexity: Message;
+  mediumUncertainty: Message;
+  highUncertainty: Message;
+  mediumEffort: Message;
+  highEffort: Message;
+
+  doNotProceed: Message;
+  average: Message;
+  allClear: Message;
+};
+
+export type MessageType = keyof StoryPointsMessages;
+
+export type StoryPointsResult = {
+  storyPoints: number;
+  message: Message;
+};
