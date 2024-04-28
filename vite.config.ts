cc360ededebccfb28a181c33cfc607d56fdd42f7
william-cesar <william.cesar.mg@hotmail.com>
2024-04-28 16:17:39 -0300

    chore: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@views': path.resolve(__dirname, './src/views')
     }
   }
 });
