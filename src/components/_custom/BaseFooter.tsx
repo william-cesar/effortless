7d124d4fa31457dc6f093f358a5f1df3bcc64412
william-cesar <william.cesar.mg@hotmail.com>
2024-04-29 23:59:45 -0300

    chore: update version to 1.0.0

--- src/components/_custom/BaseFooter.tsx
+++ src/components/_custom/BaseFooter.tsx
@@ -1,7 +1,7 @@
 const BaseFooter = () => {
   return (
     <footer className='py-4 px-8 bg-secondary h-20 fixed bottom-0 w-full flex justify-end items-center text-sm text-muted-foreground'>
-      Effortless - version 0.0.1
+      Effortless - version 1.0.0
     </footer>
   );
 };
